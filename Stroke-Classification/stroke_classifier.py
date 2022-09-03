b725ef8aefc9ec565fca62fdb0717ae5980cab30
Aiden Gandhi <90153180+000600@users.noreply.github.com>
2022-09-02 18:07:37 -0600

    Update stroke_classifier.py

--- Stroke-Classification/stroke_classifier.py
+++ Stroke-Classification/stroke_classifier.py
@@ -9,7 +9,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
-from sklearn.preprocessing import StandardScaler
 from imblearn.over_sampling import SMOTE
 
 # Load dataset
